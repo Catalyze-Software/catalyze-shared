802d265a8da686fc341a4270e07a374218e2d104
Semen Loktionov <loktionov.kh@gmail.com>
2024-07-22 11:36:02 +0300

    fix: canister method signature for the storage client to omit expects in the client code

--- src/traits/storage_client.rs
+++ src/traits/storage_client.rs
@@ -4,44 +4,44 @@ use crate::{helpers::ic_call::ic_call, CanisterResult};
 
 pub trait StorageClient<K, V, F>: Send + Sync
 where
-    K: candid::CandidType + for<'a> candid::Deserialize<'a>,
-    V: candid::CandidType + for<'a> candid::Deserialize<'a>,
-    F: candid::CandidType + Clone,
+    K: candid::CandidType + for<'a> candid::Deserialize<'a> + Sync + Send,
+    V: candid::CandidType + for<'a> candid::Deserialize<'a> + Sync + Send,
+    F: candid::CandidType + Clone + Sync + Send,
 {
-    fn canister(&self) -> Principal;
+    fn canister(&self) -> CanisterResult<Principal>;
 
     fn get(
         &self,
         id: K,
     ) -> impl std::future::Future<Output = CanisterResult<(K, V)>> + Sync + Send {
-        ic_call(self.canister(), "get", (id,))
+        async move { ic_call(self.canister()?, "get", (id,)).await }
     }
 
     fn get_many(
         &self,
         keys: Vec<K>,
     ) -> impl std::future::Future<Output = CanisterResult<Vec<(K, V)>>> + Sync + Send {
-        ic_call(self.canister(), "get_many", (keys,))
+        async move { ic_call(self.canister()?, "get_many", (keys,)).await }
     }
 
     fn get_all(
         &self,
     ) -> impl std::future::Future<Output = CanisterResult<Vec<(K, V)>>> + Sync + Send {
-        ic_call(self.canister(), "get_all", ())
+        async move { ic_call(self.canister()?, "get_all", ()).await }
     }
 
     fn find(
         &self,
         filters: Vec<F>,
     ) -> impl std::future::Future<Output = CanisterResult<Option<(K, V)>>> + Sync + Send {
-        ic_call(self.canister(), "find", (filters,))
+        async move { ic_call(self.canister()?, "find", (filters,)).await }
     }
 
     fn filter(
         &self,
         filters: Vec<F>,
     ) -> impl std::future::Future<Output = CanisterResult<Vec<(K, V)>>> + Sync + Send {
-        ic_call(self.canister(), "filter", (filters,))
+        async move { ic_call(self.canister()?, "filter", (filters,)).await }
     }
 
     fn insert(
@@ -49,7 +49,7 @@ where
         key: K,
         value: V,
     ) -> impl std::future::Future<Output = CanisterResult<(K, V)>> + Sync + Send {
-        ic_call(self.canister(), "insert", (key, value))
+        async move { ic_call(self.canister()?, "insert", (key, value)).await }
     }
 
     fn update(
@@ -57,20 +57,20 @@ where
         key: K,
         value: V,
     ) -> impl std::future::Future<Output = CanisterResult<(K, V)>> + Sync + Send {
-        ic_call(self.canister(), "update", (key, value))
+        async move { ic_call(self.canister()?, "update", (key, value)).await }
     }
 
     fn remove(
         &self,
         key: K,
     ) -> impl std::future::Future<Output = CanisterResult<bool>> + Sync + Send {
-        ic_call(self.canister(), "remove", (key,))
+        async move { ic_call(self.canister()?, "remove", (key,)).await }
     }
 
     fn remove_many(
         &self,
         keys: Vec<K>,
     ) -> impl std::future::Future<Output = CanisterResult<()>> + Sync + Send {
-        ic_call(self.canister(), "remove_many", (keys,))
+        async move { ic_call(self.canister()?, "remove_many", (keys,)).await }
     }
 }
