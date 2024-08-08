698233f834f1eaa1ba11c0b1abe0312663f78b57
Semen Loktionov <loktionov.kh@gmail.com>
2024-08-08 14:26:57 +0300

    feat: impl into vec for the boosted filter

--- src/models/boosted.rs
+++ src/models/boosted.rs
@@ -108,3 +108,9 @@ impl Filter<u64, Boost> for BoostedFilter {
         }
     }
 }
+
+impl From<BoostedFilter> for Vec<BoostedFilter> {
+    fn from(val: BoostedFilter) -> Self {
+        vec![val]
+    }
+}
