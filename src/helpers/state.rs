c81370d2598fcf9d923fa417982870c92d44d25f
Semen Loktionov <loktionov.kh@gmail.com>
2024-08-09 12:24:40 +0300

    feat: add btree storage initers

--- src/helpers/state.rs
+++ src/helpers/state.rs
@@ -2,10 +2,10 @@ use std::{cell::RefCell, fmt::Display, thread::LocalKey};
 
 use ic_stable_structures::{
     memory_manager::{MemoryId, MemoryManager},
-    Cell, DefaultMemoryImpl, Storable,
+    Cell, DefaultMemoryImpl, StableBTreeMap, Storable,
 };
 
-use crate::{CellStorageRef, MemoryManagerStorage};
+use crate::{CellStorageRef, MemoryManagerStorage, StorageRef};
 
 pub fn init_memory_manager() -> MemoryManagerStorage {
     RefCell::new(MemoryManager::init(DefaultMemoryImpl::default()))
@@ -16,8 +16,16 @@ pub fn init_cell<S: Display, V: 'static + Clone + Storable>(
     name: S,
     id: MemoryId,
 ) -> CellStorageRef<V> {
-    RefCell::new(
-        Cell::init(memory_manager.with(|p| p.borrow().get(id)), None)
-            .unwrap_or_else(|_| panic!("Failed to initialize {name} cell")),
-    )
+    let cell = Cell::init(memory_manager.with(|p| p.borrow().get(id)), None)
+        .unwrap_or_else(|_| panic!("Failed to initialize {name} cell"));
+    RefCell::new(cell)
+}
+
+pub fn init_btree<K: Storable + Ord + Clone, V: Storable>(
+    memory_manager: &'static LocalKey<MemoryManagerStorage>,
+    id: MemoryId,
+) -> StorageRef<K, V> {
+    RefCell::new(StableBTreeMap::init(
+        memory_manager.with(|p| p.borrow().get(id)),
+    ))
 }
