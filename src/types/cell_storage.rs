b28f079474544be872b10355d5a31da0f1a7fb82
Semen Loktionov <loktionov.kh@gmail.com>
2024-08-09 11:42:57 +0300

    feat: add generic cell storage

---
+++ src/types/cell_storage.rs
@@ -0,0 +1,24 @@
+use ic_stable_structures::Storable;
+
+use crate::{CellStorage, StaticCellStorageRef};
+
+pub struct GenericCellStorage<V: 'static + Clone + Storable> {
+    name: String,
+    storage: StaticCellStorageRef<V>,
+}
+
+impl<V: 'static + Clone + Storable> GenericCellStorage<V> {
+    pub fn new(name: String, storage: StaticCellStorageRef<V>) -> Self {
+        Self { name, storage }
+    }
+}
+
+impl<V: 'static + Clone + Storable> CellStorage<V> for GenericCellStorage<V> {
+    fn name(&self) -> String {
+        self.name.clone()
+    }
+
+    fn storage(&self) -> StaticCellStorageRef<V> {
+        self.storage
+    }
+}
