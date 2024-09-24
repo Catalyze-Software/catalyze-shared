3bda7517fc1b98e17da88789a31feb2d6d6a8f13
Remco <remco@r3m.codes>
2024-09-24 15:11:05 +0200

    modify ic_call

--- src/helpers/ic_call.rs
+++ src/helpers/ic_call.rs
@@ -1,24 +1,23 @@
 use candid::{utils::ArgumentEncoder, Principal};
+use serde::de::DeserializeOwned;
 
 use crate::{api_error::ApiError, CanisterCallResult, CanisterResult};
 
-pub fn ic_call<A: ArgumentEncoder, R: candid::CandidType + for<'a> candid::Deserialize<'a>>(
+pub async fn ic_call<A: ArgumentEncoder, R: candid::CandidType + DeserializeOwned>(
     canister: Principal,
     method: &str,
     args: A,
-) -> impl std::future::Future<Output = CanisterResult<R>> + Sync + Send {
-    let fut = ic_cdk::call::<A, (CanisterCallResult<R>,)>(canister, method, args);
+) -> CanisterResult<R> {
+    let fut = ic_cdk::call::<A, (CanisterCallResult<R>,)>(canister, method, args).await;
 
     let method: String = method.to_string();
 
-    async move {
-        let (res,) = fut.await.map_err(|e| {
-            ApiError::unexpected()
-                .add_message("Failed to call canister")
-                .add_info(format!("Canister: {canister} error: {:?}", e).as_str())
-                .add_method_name(method)
-        })?;
+    let (result,) = fut.map_err(|e| {
+        ApiError::unexpected()
+            .add_message("Failed to call canister")
+            .add_info(format!("Canister: {canister} error: {:?}", e).as_str())
+            .add_method_name(method)
+    })?;
 
-        res.into()
-    }
+    result.into()
 }
